574b77be0ae971f05d9c0c7ec474e03fbcd803c2
wanghaotianGitHub <m19834325668@163.com>
2019-04-23 15:24:48 +0000

    ll

--- config/database.php
+++ config/database.php
@@ -116,7 +116,7 @@ return [
         'client' => env('REDIS_CLIENT', 'phpredis'),
 
         'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'predis'),
+            'cluster' => env('REDIS_CLUSTER', 'phpredis'),
         ],
 
         'default' => [
@@ -126,14 +126,11 @@ return [
             'database' => env('REDIS_DB', 0),
             'prefix' => '1809a_',
         ],
-
         'cache' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_CACHE_DB', 1),
         ],
-
     ],
-
 ];
