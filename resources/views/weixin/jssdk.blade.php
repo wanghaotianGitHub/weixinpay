cbf31b0e6c80730dfc6f86610721e4637ce2efd2
wanghaotianGitHub <m19834325668@163.com>
2019-04-23 15:36:38 +0000

    ik

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -19,7 +19,7 @@
 <hr>
 <img src="" alt="" id="imgs2"  width="300">
 
-<script src="/js/jquery/jquery-3.2.1.min.js"></script>
+<script src="/js/jquery/jquery-1.12.4.min.js"></script>
 <script src="http://res2.wx.qq.com/open/js/jweixin-1.4.0.js "></script>
 <script>
     wx.config({
