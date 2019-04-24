13226c04cce8b5d4a81eec4f3c192b8ab5bcb596
wanghaotianGitHub <m19834325668@163.com>
2019-04-24 14:19:59 +0000

    推送

--- app/Http/Controllers/Weixin/WxController.php
+++ app/Http/Controllers/Weixin/WxController.php
@@ -169,8 +169,9 @@ class WxController extends Controller
         //获取 access_token
         $url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APPID').'&secret='.env('WX_SECRET').'&code='.$code.'&grant_type=authorization_code';
         $response = json_decode(file_get_contents($url),true);
-        echo '<pre>';print_r($response);echo '</pre>';die;
+        echo '<pre>';print_r($response);echo '</pre>';
         $access_token = $response['access_token'];
+        echo $access_token;die;
         $openid = $response['openid'];
         //获取用户信息
         $url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
